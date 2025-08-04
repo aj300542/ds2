5bcb5af574f5f8f917cc07becf88ac3810cf0bdf
aj300 <165155838+aj300542@users.noreply.github.com>
2025-08-04 22:47:14 +0800

    Add files via upload

--- html/script.js
+++ html/script.js
@@ -14,6 +14,7 @@ window.onload = () => {
     const wormContainer = document.getElementById("wormContainer");
     const worms = [];
     const scaleFactor = 0.64;
+
     const wormConfigs = [
         { speed: 0.0004, start: 0.05, phase: 0, amplitude: 20, headScale: 0.28, bodyScale: 0.56, tailScale: 0.21, color: "radial-gradient(circle at 40% 60%, rgba(47, 248, 255, 0.2), rgba(255,20,147,0.15))" },
         { speed: 0.00056, start: 0.125, phase: Math.PI / 2, amplitude: 35, headScale: 0.42, bodyScale: 0.56, tailScale: 0.28, color: "radial-gradient(circle at 40% 60%, rgba(0,255,255,0.2), rgba(255,182,193,0.25))" },
@@ -22,17 +23,20 @@ window.onload = () => {
         { speed: 0.0007, start: 0.35, phase: Math.PI * 1.5, amplitude: 35, headScale: 0.35, bodyScale: 0.77, tailScale: 0.49, color: "radial-gradient(circle at 40% 60%, rgba(255,20,147,0.1), rgba(0,255,255,0.2))" },
         { speed: 0.0005, start: 0.425, phase: Math.PI / 6, amplitude: 18, headScale: 0.56, bodyScale: 0.77, tailScale: 0.42, color: "radial-gradient(circle at 40% 60%, rgba(245, 229, 200, 0.15), rgba(255,105,180,0.3))" },
         { speed: 0.0006, start: 0.5, phase: Math.PI * 1.2, amplitude: 28, headScale: 0.49, bodyScale: 0.7, tailScale: 0.35, color: "radial-gradient(circle at 40% 60%, rgba(255,105,180,0.3), rgba(210,240,240,0.2))" },
-        { speed: 0.00044, start: 0.575, phase: Math.PI / 4, amplitude: 22, headScale: 0.14, bodyScale: 0.42, tailScale: 0.28, color: "radial-gradient(circle at 40% 60%, rgba(235,255,255,0.15), rgba(200,255,200,0.15))" },
-        { speed: 0.00076, start: 0.65, phase: Math.PI * 0.75, amplitude: 20, headScale: 0.63, bodyScale: 0.42, tailScale: 0.42, color: "radial-gradient(circle at 40% 60%, rgba(255,255,255,0.1), rgba(0,255,255,0.2))" },
-        { speed: 0.00042, start: 0.725, phase: Math.PI / 8, amplitude: 16, headScale: 0.448, bodyScale: 0.672, tailScale: 0.308, color: "radial-gradient(circle at 40% 60%, rgba(235,235,255,0.22), rgba(180,230,255,0.15))" },
-        { speed: 0.0008, start: 0.8, phase: Math.PI / 1.7, amplitude: 17, headScale: 0.532, bodyScale: 0.7, tailScale: 0.392, color: "radial-gradient(circle at 40% 60%, rgba(173,255,47,0.2), rgba(255, 200, 223, 0.15))" },
-        { speed: 0.001, start: 0.875, phase: Math.PI * 1.8, amplitude: 12, headScale: 0.364, bodyScale: 0.728, tailScale: 0.336, color: "radial-gradient(circle at 40% 60%, rgba(245,255,245,0.12), rgba(255,105,180,0.3))" }
+        { speed: 0.00044, start: 0.595, phase: Math.PI / 4, amplitude: 22, headScale: 0.14, bodyScale: 0.42, tailScale: 0.28, color: "radial-gradient(circle at 40% 60%, rgba(235,255,255,0.15), rgba(200,255,200,0.15))" },
+        { speed: 0.00076, start: 0.68, phase: Math.PI * 0.75, amplitude: 20, headScale: 0.63, bodyScale: 0.42, tailScale: 0.42, color: "radial-gradient(circle at 40% 60%, rgba(255,255,255,0.1), rgba(0,255,255,0.2))" },
+        { speed: 0.00042, start: 0.785, phase: Math.PI / 8, amplitude: 16, headScale: 0.448, bodyScale: 0.672, tailScale: 0.308, color: "radial-gradient(circle at 40% 60%, rgba(235,235,255,0.22), rgba(180,230,255,0.15))" },
+        { speed: 0.0008, start: 0.85, phase: Math.PI / 1.7, amplitude: 17, headScale: 0.532, bodyScale: 0.7, tailScale: 0.392, color: "radial-gradient(circle at 40% 60%, rgba(173,255,47,0.2), rgba(255, 200, 223, 0.15))" },
+        { speed: 0.001, start: 0.975, phase: Math.PI * 1.8, amplitude: 12, headScale: 0.364, bodyScale: 0.728, tailScale: 0.336, color: "radial-gradient(circle at 40% 60%, rgba(245,255,245,0.12), rgba(255,105,180,0.3))" }
     ].map(config => ({
         ...config,
         headScale: config.headScale * scaleFactor,
         bodyScale: config.bodyScale * scaleFactor,
         tailScale: config.tailScale * scaleFactor
     }));
+
+
+
 
     function createWorm(cfg, index) {
         const tail = document.createElement("div");
@@ -86,8 +90,10 @@ window.onload = () => {
     });
 
     function fadeCanvas() {
-        ctx.fillStyle = "rgba(0, 0, 0, 0.008)"; // 越小残影越长，越大越快消散
+        ctx.globalCompositeOperation = "destination-out";
+        ctx.fillStyle = "rgba(0,0,0,0.01)";
         ctx.fillRect(0, 0, canvas.width, canvas.height);
+        ctx.globalCompositeOperation = "source-over"; // 或 "source-over lighter "
     }
 
     function updateWorm(worm, index) {
